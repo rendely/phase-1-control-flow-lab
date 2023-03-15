8570de6e3b3697473a5e4eaeb849588594c256c7
matthew rendely <matthew.rendely@gmail.com>
2023-03-14 19:57:07 -0700

    done

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(rideDistanceInFeet) {
+  if (rideDistanceInFeet <= 400) {
+    return 'This one is on me!'
+  } else if (rideDistanceInFeet > 400 && rideDistanceInFeet <= 2000) {
+    return 'That will be twenty bucks.'
+  } else if (rideDistanceInFeet > 2000 && rideDistanceInFeet <= 2500) {
+    return 'I will gladly take your thirty bucks.'
+  } else {
+    return 'No can do.'
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city) {
+  return city === "NYC" ? 'Ok, sounds good.' : 'No go.'
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tipQuality) {
+  switch (tipQuality) {
+    case 'generous':
+      return 'Thank you so much.';
+    case 'not as generous':
+      return 'Thank you.';
+    default:
+      return 'Bye.';
+  }
 }
